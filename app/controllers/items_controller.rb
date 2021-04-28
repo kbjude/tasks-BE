8ff0ba9bec08ae4993c1b21a3b3b56ac2b965fcf
kbjude <kbjude@gmail.com>
2021-04-28 23:39:48 +0300

    Working items components

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,36 @@
 class ItemsController < ApplicationController
+    def index
+        @items = Item.all
+        json_response(@items)
+    end
+
+    def create
+        @item = Item.create!(item_params)
+        json_response(@items, :created)
+    end
+
+    def show
+        json_response(@item)
+    end
+
+    def update
+        @item.update(item_params)
+        head :no_content
+    end
+
+    def destroy
+        @item.destroy
+        head :no_content
+    end
+
+    private
+
+    def item_params
+      # whitelist params
+      params.permit(:name, :created_by)
+    end
+  
+    def set_item
+      @item = Item.find(params[:id])
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,41 +1,5 @@
 class Item < ApplicationRecord
-    before_action :set_todo, only: [:show, :update, :destroy]
-    has_many :itemdetails, dependent: :destroy
-    validates_presence_of :name
-
-    def index
-        @items = Items.all
-        json_response(@items)
-    end
-
-    def create
-        @item = Item.create!(item_params)
-        json_response(@items, :created)
-    end
-
-    def show
-        json_response(@item)
-    end
-
-    def update
-        @item.update(item_params)
-        head :no_content
-    end
-
-    def destroy
-        @item.destroy
-        head :no_content
-    end
-
-    private
-
-    def item_params
-      # whitelist params
-      params.permit(:name, :created_by)
-    end
-  
-    def set_item
-      @item = Item.find(params[:id])
-    end
-    
+    # before_action :set_todo, only: [:show, :update, :destroy]
+    # has_many :itemdetails, dependent: :destroy
+    # validates_presence_of :name
 end
