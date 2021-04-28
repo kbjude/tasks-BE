b2fae17ccb0af004ea2f88669905e9a201e83a5a
kbjude <kbjude@gmail.com>
2021-04-28 23:48:21 +0300

    Fetch working fine

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-    # before_action :set_todo, only: [:show, :update, :destroy]
-    # has_many :itemdetails, dependent: :destroy
-    # validates_presence_of :name
+    # before_action :set_item, only: [:show, :update, :destroy]
+    has_many :itemdetails, dependent: :destroy
+    validates_presence_of :name
 end
